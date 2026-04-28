70c234b93a18c805719b3a4599d71529cc480960
Dilliya <dilusha.37.writer@gmail.com>
2026-04-28 17:41:01 +0400

    do 1

--- static/focus.js
+++ static/focus.js
@@ -5,8 +5,8 @@ const API = {
     buhForms: "/api3/buh",
 };
 
-function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
+async function run() {
+    await sendRequest(API.organizationList, (orgOgrns) => {
         const ogrns = orgOgrns.join(",");
         sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
             const orgsMap = reqsToMap(requisites);
@@ -24,18 +24,36 @@ function run() {
 run();
 
 function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
+    // const xhr = new XMLHttpRequest();
+    // xhr.open("GET", url, true);
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
+    // xhr.onreadystatechange = function () {
+    //     if (xhr.readyState === XMLHttpRequest.DONE) {
+    //         if (xhr.status === 200) {
+    //             callback(JSON.parse(xhr.response));
+    //         }
+    //     }
+    // };
+
+    // xhr.send();
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    const result = (JSON.parse(xhr.response));
+                    callback(result);
+                    resolve(result);
+                } else {
+                    reject(new Error(`Request failed with status ${xhr.status}`));
+                }
             }
-        }
-    };
-
-    xhr.send();
+        };
+        xhr.onerror = () => reject(new Error("Network Error"));
+        xhr.send();
+    });
 }
 
 function reqsToMap(requisites) {
@@ -86,7 +104,7 @@ function renderOrganization(orgInfo, template, container) {
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0] &&
                 orgInfo.buhForms[orgInfo.buhForms.length - 1].form2[0]
                     .endValue) ||
-                0
+            0
         );
     } else {
         money.textContent = "—";
